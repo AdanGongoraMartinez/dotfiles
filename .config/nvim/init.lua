43aa74b7421de33e4fd3dbb5f4dce929592637f9
Adán Góngora Martínez <21890241@ittizimin.edu.mx>
2025-11-25 01:05:38 -0600

    add: hidden files detecion for telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -438,6 +438,12 @@ require('lazy').setup({
             require('telescope.themes').get_dropdown(),
           },
         },
+        file_ignore_patterns = {
+          '.git/*',
+          'node_modules',
+          '__pycache__',
+          'build',
+        },
       }
 
       -- Enable Telescope extensions if they are installed
@@ -456,6 +462,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sH', function()
+        require('telescope.builtin').find_files {
+          hidden = true,
+        }
+      end, { desc = '[S]earch Files [H]idden' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
